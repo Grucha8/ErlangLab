7462e2d885200714bd0e2ea97de1edc650ae571e
Grucha8 <t.szamot1997@gmail.com>
2018-04-23 11:56:41 +0200

    End of phase one

--- src/pollution.erl
+++ src/pollution.erl
@@ -3,6 +3,7 @@
 %%% @copyright (C) 2018, <COMPANY>
 %%% @doc Module to monitor the pollution of air. Struct to store date is:
 %%% #{ {StationName, {Xcord, Ycord}} => #{ {Date, Type} => Value } }
+%%% Date = {Y, M, D, H}
 %%%
 %%% @end
 %%% Created : 22. kwi 2018 23:13
@@ -11,7 +12,11 @@
 -author("tkarkocha").
 
 %% API
--export([createMonitor/0, addStation/3]).
+-export([createMonitor/0, addStation/3, addValue/5, removeValue/4, getOneValue/4]).
+-export([getStationMean/3, getDailyMean/3]).
+
+
+-define(DATE, {{Year, Month, Day}, {Hour, _, _}}).
 
 %% @doc Function to create a monitor which will
 %% monitor the pollution stations
@@ -22,27 +27,66 @@ createMonitor() ->
 %% @param
 addStation(StationName, {X, Y}, M) ->
   case checkKey(maps:keys(M), StationName, {X, Y}) of
-    true -> io:format("This station already exists!"), M;
+    true -> io:format("This station already exists!~n"), M;
     false -> M#{{StationName, {X,Y}} => #{}}
   end.
 
-
-
 %% @doc Function to add new reading to monitor
-addValue(Id, Date, Type, Value, M) ->
+addValue(Id, ?DATE, Type, Value, M) ->
   FullId = retFullId(Id, M),
+  Date = {Year, Month, Day, Hour},
   case checkKey(maps:keys(M), Id) and                         %if exits this station
-       ( not checkKey(maps:keys(maps:get(FullId)), {Date, Type}) ) of
+       ( not checkKey(maps:keys(maps:get(FullId, M)), {Date, Type}) ) of
     false -> io:format("This record cant be added"), M;
     true  -> M#{ FullId => maps:put({Date, Type}, Value, maps:get(FullId, M)) }
   end.
 
+%% @doc Function to remove an existing reading from monitor.
+removeValue(Id, ?DATE, Type, M) ->
+  Date = {Year, Month, Day, Hour},
+  FullId = retFullId(Id, M),
+  M#{ FullId => maps:remove({Date, Type}, maps:get(FullId, M)) }.
 
-retFullId(Coord, [{N, C} | T]) when Coord == C -> {N, C};
+%% @doc Function to get value of certain reading
+getOneValue(Id, ?DATE, Type, M) ->
+  Date = {Year, Month, Day, Hour},
+  FullId = retFullId(Id, M),
+  maps:get({Date, Type}, maps:get(FullId, M)).
+
+%% @doc Function to return the average value
+getStationMean(Id, Type, M) ->
+  FullId = retFullId(Id, M),
+  {Sum, I, _} = maps:fold(fun valuesFun/3, {0, 0, Type}, maps:get(FullId, M)),
+  Sum / I.
+
+valuesFun({_, Type}, V, {Acc, I, PrimType}) when Type == PrimType ->
+  {Acc + V, I + 1, PrimType};
+
+valuesFun(_, _, {Acc, I, PrimType}) -> {Acc, I, PrimType}.
+
+%% @doc Function which returns the average
+getDailyMean(Type, Day, M) ->
+  {Sum, I, _} = maps:fold(fun dailyMean/3, {0, 0, {Day, Type}}, M),
+  Sum / I.
+
+dailyMean({{_,_,D,_}, Type}, V, {Acc, I, {Day, Type_}})
+  when (D == Day) and (Type == Type_) ->
+  {Acc + V, I + 1, {Day, Type_}};
+
+dailyMean(_, V, {Acc, I, W}) when is_map(V) ->
+  maps:fold(fun dailyMean/3, {Acc, I, W}, V);
+
+dailyMean(_, _, {Acc, I, W}) -> {Acc, I, W}.
+
+
+%% =================================================== %%
+retFullId(Name, M) when is_map(M) -> retFullId(Name, maps:keys(M));
 
 retFullId(Name, [{N, C} | T]) when Name == N -> {N, C};
 
-retFullId(Name, [H | T]) -> returnOtherPartOfId(Name, T);
+retFullId(Coord, [{N, C} | T]) when Coord == C -> {N, C};
+
+retFullId(Name, [H | T]) -> retFullId(Name, T);
 
 retFullId(_, []) -> error("Not like this").
 
